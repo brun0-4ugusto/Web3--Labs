95d0d896621b1821711359af9fb2d79ad58dc14f
Bruno Augusto <b.augusto1501@gmail.com>
2022-05-11 00:31:02 -0300

    Basic Setup: Truffle and React. And Lottery Contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Lottery {
+    event NewPlayerAdd(address _player);
+    address payable[] public players;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Nops, only the owner");
+        _;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function enter() external payable {
+        require(msg.value == 10000 wei, "Fail. Send the right amount");
+        players.push(payable(msg.sender));
+        emit NewPlayerAdd(msg.sender);
+    }
+
+    function pickWinner(string memory _secretWord) external onlyOwner {
+        require (players.length >= 3);
+        address payable winner;
+        uint number = pseudoRandomNumber(_secretWord);
+        uint index = number % players.length;
+        winner = players[index];
+        uint managerFee = (getBalance() * 10 ) / 100;
+        uint winnerPrize = (getBalance() * 90 ) / 100;
+
+        (bool success,) = winner.call{value:winnerPrize}("");
+        require(success, "Failed");
+        (bool sent,) = payable(owner).call{value:managerFee}("");
+        require(sent, "Failed");
+        players = new address payable[](0);
+        
+    }
+
+    function pseudoRandomNumber(string memory _secretWord)
+        internal
+        view
+        returns (uint256)
+    {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        players.length,
+                        _secretWord
+                    )
+                )
+            );
+    }
+}
---
+++ frontend-labs/src/routes.js
@@ -0,0 +1,10 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+
+export default function AppRouter(){
+    return(<Router>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+        </Routes>
+    </Router>);
+}
