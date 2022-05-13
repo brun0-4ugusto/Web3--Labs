013a02b9aa6225fcd39e13543f23de1086d1ee2e
Bruno Augusto <b.augusto1501@gmail.com>
2022-05-13 17:14:34 -0300

    Header Style

--- frontend-labs/src/Pages/Home/index.tsx
+++ frontend-labs/src/Pages/Home/index.tsx
@@ -1,3 +1,43 @@
-export default function Home(){
-    return <h1>Index</h1>
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Web3 from "web3";
+import getWeb3 from "../../Assets/Web3/getWeb3";
+import logo from "../../Assets/images/logo.png"
+import styles from "./home.module.css";
+
+const rotas = [
+  {
+    label: "lottery",
+    to: "/",
+  },
+];
+
+export default function Home() {
+  const [connection, setConnection] = useState<string>("Não Conectado");
+
+  useEffect(() => {
+    getWeb3()
+      .then((web3: Web3) => {
+        web3.eth.getAccounts().then((data) => setConnection(data[0]));
+      })
+      .catch((web3: Web3) => {
+        console.log(web3);
+      });
+  }, []);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__logo}>CryptoLab</div> 
+      <nav>
+        <ul>
+          {rotas.map((rota, index) => (
+            <li key={index}>
+              <Link to={rota.to}>{rota.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div>{connection}</div>
+    </header>
+  );
 }
--- frontend-labs/src/index.tsx
+++ frontend-labs/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './routes';
-
+import 'normalize.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
