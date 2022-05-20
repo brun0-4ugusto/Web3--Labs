d44e361bf6eefbbbb9f268fea5609fc9efe9a08e
Bruno Augusto <b.augusto1501@gmail.com>
2022-05-19 23:31:59 -0300

    LabCoin Contract

---
+++ contracts/LabCoin.sol
@@ -0,0 +1,95 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "Web3--Labs/contracts/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract ERC20 is IERC20{
+    string public name = "LabCoin";
+    string public symbol = "CLC";
+    uint8 public decimals = 18;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    uint256 public totalSupply;
+
+    constructor(){
+        totalSupply += 1000 * (10**18);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        address owner = msg.sender;
+        _transfer(owner, to, amount);
+        return true;
+    }
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) private {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+        uint256 fromBalance = balanceOf[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            balanceOf[from] = fromBalance - amount;
+        }
+        balanceOf[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+        
+    }
+    function approve(address spender, uint256 amount) public returns (bool) {
+        address owner = msg.sender;
+        _approve(owner, spender, amount);
+        return true;
+    }
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) private {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public returns (bool) {
+        address spender = msg.sender;
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+    
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) private {
+        uint256 currentAllowance =  allowance[owner][spender];
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+            unchecked {
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
+
+    function mint() external payable returns(bool) {
+        require(msg.value == 0.025 ether, "Para participar envie 0.025 ether");
+        uint amount = 1000 * (10**decimals);
+        totalSupply += amount;
+        balanceOf[msg.sender] += amount;
+        emit Transfer(address(0), msg.sender, amount);
+        return true;        
+    }
+
+    
+    
+}
